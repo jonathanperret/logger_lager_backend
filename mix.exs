bbd567a0398d7b336d19304afa925e291a4139c1
Jonathan Perret <jonathan@ut7.fr>
2018-04-19 16:46:20 +0200

    Bump to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LoggerLagerBackend.Mixfile do
   def project do
     [app: :logger_lager_backend,
      description: "A Logger backend that forwards messages to lager",
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
